6934ff01c5c484971015d64bad321e9e640306be
Patrik Pančišin <pancisin.patrik@hotmail.com>
2018-09-11 11:01:47 +0200

    DIsabled cors for oauth/token

--- src/main/kotlin/com/pancisin/webappcore/configuration/ResourceServerConfig.kt
+++ src/main/kotlin/com/pancisin/webappcore/configuration/ResourceServerConfig.kt
@@ -33,7 +33,8 @@ open class ResourceServerConfig : ResourceServerConfigurerAdapter() {
       .and()
       .authorizeRequests()
       .anyRequest().permitAll()
-      .antMatchers("/actuator/**", "/api-docs/**", "/api/register", "/oauth/token").permitAll()
+      .antMatchers("/actuator/**", "/api-docs/**", "/api/register").permitAll()
+      .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
       .antMatchers("/api/v1/**").authenticated()
       .and().cors().disable()
   }
